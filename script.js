27b6a18aab8d14115ab97deb0917b8a2a23e7919
fabbe312 <familjenpossitv@gmail.com>
2024-06-24 06:25:11 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
   particlesJS.load('particles-js', 'particles.json', function() {
     console.log('particles.js loaded - callback');
-  });
-});
